5f0d3150b0fa85713154d0a807f4c9ae4d1c554a
Lyubov Golubeva <golubeva.lyubov@gmail.com>
2020-12-04 18:50:49 +0300

    lesson01 task03

--- lesson01/task_3.py
+++ lesson01/task_3.py
@@ -11,3 +11,18 @@
 {'elem_18': 18, 'elem_22': 22, 'elem_21': 21, 'elem_23': 23, 'elem_19': 19, 'elem_16': 16, 'elem_8': 8}
 )
 """
+
+import random
+
+
+def random_generator(start, finish):
+    generated_list = []
+    generated_dict = {}
+    for i in range(10):
+        random_number = int((finish - start) * random.random() + start)
+        generated_list.append(random_number)
+        generated_dict[f'elem_{format(random_number)}'] = random_number
+
+    return (generated_list, generated_dict)
+
+print(random_generator(5,15))
